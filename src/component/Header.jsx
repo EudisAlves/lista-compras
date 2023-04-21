72d2f4a031b7d0389a9142a603f3356e66550b6b
Eudis Alves <deleudis@gmail.com>
2023-04-20 21:26:52 -0300

    refatorando

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,7 +3,7 @@ import "../css/header.css"
 export default function Header() {
     return (
         <div className="header">
-        <h1>Eudis Alves</h1>
+            <h1>Eudis Alves</h1>
         </div>
     )
 }
